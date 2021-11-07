502feb232d3d80aa2938d28998559322bc6bd390
Alix Fachin <alix.fachin@me.com>
2021-11-07 18:50:57 +0900

    Adding a rule aregarding space around operators, changing cmdline syntax

--- riotlinter.js
+++ riotlinter.js
@@ -11,12 +11,12 @@
 //  AFTER -> Displays the original line number (so that we can find the mistake afterwards)
 // A rule -> A function which returns "true" if there is a match 
 
-// having a "getLineNumber" which looks for the line number of a given expression inside a file
 
 // RULES
 // - no backquotes
-// only single quotes in JavaScript
-// space around operators in expression (HTML and JS)
+// DONE - only single quotes in JavaScript
+// space around operators in expression (JS BLOCK)
+// space around operators in an expression (HTML BLOCK)
 // Issue a warning when the three equals are used
 // White space inside {} for HTML expressions
 // White space outside for IF and FOR
@@ -27,33 +27,38 @@
 // Making sure functions have commented all the parameters (-> Not sure really needed)
 // Missing semicolumns (? -> This one will be tricky!)
 
-const noBackQuote = (block) => {
-    const errorList = [];
-    lines = block.split('\n');
-    pattern = new RegExp("`");
-    for (line of lines) {
-        if (pattern.test(line)) {
-            errorList.push(line);
-        } 
-    }
-    return errorList;
-};
-noBackQuote.message = 'Forbidden use of backquotes!:';
+// 
 
-const noDoubleQuote = (block) => {
-    const errorList = [];
-    lines = block.split('\n');
-    pattern = new RegExp("`");
-    for (line of lines) {
-        if (pattern.test(line)) {
-            errorList.push(line);
+// simpleRegExpRuleFactory returns an "rule" object, with a function and a message
+const simpleRegExpRuleFactory = (regExpString, errorMessage) => {
+    const rule = {};
+    rule.message = errorMessage;
+    rule.process = (block) => {
+        const errorList = [];
+        pattern = new RegExp(regExpString);
+        lines = block.split('\n');
+        for (let i=0; i< lines.length; i++ ) {
+            if (pattern.test(lines[i])) {
+                errorList.push({
+                    lineNr: i,
+                    errorLine: lines[i],
+                })
+            }
         }
+        return errorList
     }
-    return errorList;
+    return rule;
 }
 
+// Building the operator rule little by little
+const js_operators = '[' + ['\\+','\\-', '\\*', '%','<','>' ].join() + ']';
+const js_operators_regexp = `(\\w${js_operators}\\w | \\s${js_operators}\\w | \\w${js_operators}\\s)`
+console.log(js_operators_regexp);
+
 const jsRulesList = [
-    noBackQuote,
+    simpleRegExpRuleFactory('`', 'Forbidden use of backquotes!'),
+    simpleRegExpRuleFactory('"', 'Forbidden use of double quotes in JS!'),
+    simpleRegExpRuleFactory(js_operators_regexp, 'Not enough space around operators!'),
 ];
 
 const cssRulesList = [];
@@ -62,33 +67,38 @@ const { existsSync, readFileSync } = require("fs");
 
 // argv[0] = node
 // argv[1] = this script name
-// argv[3] = the first actual parameter
-if (process.argv.length < 3) {
+// argv[2] = the first actual parameter - file name and path
+// argv[3] = the component name
+if (process.argv.length < 4) {
     console.error('Not enough parameters!')
+    console.log('usage: node riotliner.js <path-to-riot-file> <component-name>')
     return;
 }
 
-const componentName = process.argv[2];
-const filePath = `./src/${componentName}.riot`
+const componentName = process.argv[3];
+const filePath = process.argv[2];
 if (!existsSync(filePath) ) {
     console.error('Cannot find component file!')
     return;
 }
 
 const data = readFileSync(filePath, "utf8");
-const filePattern = new RegExp( `<style>(?<css_block>(.|\n)*?)<\/style>(.|\n)*?<script>(?<js_block>(.|\n)*?)<\/script>`)
+const filePattern = new RegExp( `(?<html_block>(.|\n)*?)<style>(?<css_block>(.|\n)*?)<\/style>(.|\n)*?<script>(?<js_block>(.|\n)*?)<\/script>`)
 
 const parts = data.match(filePattern);
 if (parts) {
     const cssBlock = parts.groups['css_block'];
     const jsBlock = parts.groups['js_block'];
+    const htmlBlock = parts.groups['html_block'];
    // console.log('The css Block is:\n' + jsBlock);
 
-    let errorList = [];
-    for (let rule of jsRulesList) {
-        errorList = rule(jsBlock);
-        errorList.forEach(wrongLine => {
-            console.log(`${rule.message}:\n${wrongLine}`);
+    let errorList;
+    let rule;
+    for (let i=0; i<jsRulesList.length; i++) {
+        rule = jsRulesList[i];
+        errorList = rule.process(jsBlock);
+        errorList.forEach(ruleError => {
+            console.log(`\x1b[31m${rule.message}\x1b[0m:\n Line \x1b[33m${ruleError.lineNr}\x1b[0m:${ruleError.errorLine}`);
         });
     }
 
